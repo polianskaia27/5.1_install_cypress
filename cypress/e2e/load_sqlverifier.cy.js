0f7fd66fc85bb45abf577a69ff4d9abc1e01bde6
sasha_polianskaia <polianskaia27@gmail.com>
2024-10-25 17:26:36 -0300

    deleted changes in cypress.config.js

--- cypress/e2e/load_sqlverifier.cy.js
+++ cypress/e2e/load_sqlverifier.cy.js
@@ -1,10 +1,10 @@
 before(() => {
   cy.visit(
-    "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc"
+    "/?page=1&sort=id,asc"
   );
 });
 it("should have pagination", () => {
   cy.get(
     "#app-view-container > div.jh-card.card > div > div > div:nth-child(3) > div:nth-child(2) > div > nav"
-  ).should('be.visible');
+  ).debug().should('be.visible');
 });
--- cypress2.config.js
+++ cypress2.config.js
@@ -5,5 +5,9 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'https://google.com',
+    watchForFileChanges: false,
   },
+  projectId: "dcoyx8",
 });
+
