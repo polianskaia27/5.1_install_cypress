903d667c02cb343aefeae4916ffc05f8b7bb651a
sasha_polianskaia <polianskaia27@gmail.com>
2024-10-25 22:40:41 -0300

    changed load_sqlverifier.cy.js

--- cypress/e2e/load_sqlverifier.cy.js
+++ cypress/e2e/load_sqlverifier.cy.js
@@ -1,6 +1,6 @@
 before(() => {
   cy.visit(
-    "https://sqlverifier-live-6e21ca0ed768.herokuapp.com/?page=1&sort=id,asc"
+    "/?page=1&sort=id,asc"
   );
 });
 it("should have pagination", () => {
